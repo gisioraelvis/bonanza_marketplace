a2e69584275bc2c458714be891700852bd7014ea
Gisiora Elvis <gisioraelvis20@gmail.com>
2021-06-24 11:19:04 +0300

    register new user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,45 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import generateJWT from "../utils/generateJWT.js";
+
 /**
- * @desc    GET all users
+ * @desc    Signup a new user
+ * @route   POST /api/users/signup
+ * @access  Public
+ */
+
+export const signupNewUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateJWT(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+/**
+ * @desc    Get all users
  * @route   GET /api/users
  * @access  Public
  */
@@ -14,7 +51,7 @@ export const getUsers = asyncHandler(async (req, res) => {
 
 /**
  * @desc    Login - auth user & get token
- * @route   GET /api/user/login
+ * @route   GET /api/users/login
  * @access  Public
  */
 
@@ -22,7 +59,7 @@ export const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
-    res.send({
+    res.json({
       _id: user.id,
       name: user.name,
       email: user.email,
@@ -44,7 +81,7 @@ export const authUser = asyncHandler(async (req, res) => {
 export const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
   if (user) {
-    res.send({
+    res.json({
       _id: user.id,
       name: user.name,
       email: user.email,
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,11 +3,13 @@ import {
   getUsers,
   authUser,
   getUserProfile,
+  signupNewUser,
 } from "../controllers/userController.js";
 import protect from "../middleWares/userAuth.js";
 const router = express.Router();
 
 router.route("/").get(getUsers);
+router.route("/signup").post(signupNewUser);
 router.route("/login").post(authUser);
 router.route("/profile").get(protect, getUserProfile);
 
