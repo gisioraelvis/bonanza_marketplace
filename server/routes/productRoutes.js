70aa50ae0e1ebc899e02cf520478b0c18eab00ad
Gisiora Elvis <gisioraelvis20@gmail.com>
2021-07-02 05:29:14 +0300

    Reinitialized react setup

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,12 +2,14 @@ import express from "express";
 import {
   getProducts,
   getProductById,
+  createProductReview,
 } from "../controllers/productsController.js ";
+import protect from "../middleWares/userAuth.js";
 
 const router = express.Router();
 
 router.route("/").get(getProducts);
-
 router.route("/:id").get(getProductById);
+router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
